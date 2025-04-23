25ea32c5cfa807ca71779ba21cc37ae60bbdac86
KagamiUmi <kagamiumi9@gmail.com>
2025-04-23 04:46:33 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# Botun ayarları
+AUTO_TRADE = True         # Otomatik al-sat aktif mi?
+DEMO_MODE = True          # Gerçek para mı, sanal mı?
+MAX_LOSS_PERCENT = 0.5    # Maksimum kabul edilen zarar yüzdesi
+PROFIT_TARGET_PERCENT = 2 # Hedeflenen kar yüzdesi
+CHECK_INTERVAL = 1        # Saniyede bir kontrol et (1 saniye)
+HISTORY_FILE = "history/trade_history.json"  # İşlem geçmişi kaydı
